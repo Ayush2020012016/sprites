4af8087b3b70996c3573b18e65b1e3e276fc738b
Ayush2020012016 <ayush.2020012016@tulas.edu.in>
2023-07-03 10:42:09 +0530

    initila commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+ 
 
-function App() {
+const App = ()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    
 
+    <div>
+    <span class="fp bo">ayush</span>
+    <span style={{borderRadius:"50%",width:"20em",height:"20em"}} class="fi fi-gr fis"></span>
+     <span style={{borderRadius:"50%",width:"20em",height:"20em"}} class="fi fi-af fis"></span>
+    </div>
+
+
+    </>
+  )
+}
 export default App;
